bc9684946ef8bb8e8bc5551e109c61548f159b4b
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-03-05 10:36:05 +0530

    Create Set11_2.c

---
+++ Set11_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+    
+        a=a/2;
+        printf("%d",a);
+    }
+    
+    
+    else
+    {
+        printf("%d",a);
+    }
+    return 0;
+}
